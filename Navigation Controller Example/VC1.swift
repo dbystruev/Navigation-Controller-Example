c19738033e88fc9d0d7544975c596361edb7fbb1
Denis Bystruev <dbystruev@me.com>
2018-02-04 15:44:47 +0300

    Initial Commit

---
+++ Navigation Controller Example/VC1.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Navigation Controller Example
+//
+//  Created by Denis Bystruev on 04/02/2018.
+//  Copyright © 2018 Denis Bystruev. All rights reserved.
+//
+
+import UIKit
+
+class VC1: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
--- Navigation Controller Example/VC2.swift
+++ Navigation Controller Example/VC2.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  SecondViewController.swift
 //  Navigation Controller Example
 //
 //  Created by Denis Bystruev on 04/02/2018.
@@ -8,18 +8,18 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class VC2: UIViewController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
 
